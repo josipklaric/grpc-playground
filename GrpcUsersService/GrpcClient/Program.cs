720639a6fad8ddc47cc18781bfc059db7ef0bd6b
Josip Klaric <josip.klaric@infobip.com>
2020-06-20 17:23:07 +0200

    Basic cconsole client for greeter service example.

---
+++ GrpcUsersService/GrpcClient/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+using GrpcDemo;
+
+namespace GrpcClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("=== gRPC demo client ===");
+            
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+
+            var client = new Greeter.GreeterClient(channel);
+
+            Console.WriteLine($"Sending request to Greet service ...");
+
+            var reply = await client.SayHelloAsync(new HelloRequest { Name = "Josip" });
+
+            Console.WriteLine($"Reply from server: " + reply.Message);
+
+            //Console.ReadKey();
+        }
+    }
+}
