925aaed27471420d4d225ac7d294ad5ba6142ee9
Josip Klaric <josip.klaric@infobip.com>
2020-06-21 01:20:24 +0200

    Chat server projects with default grpc service structure added , and two empty client projects (console and wpf)

---
+++ GrpcChatService/GrpcChatClientConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GrpcChatClientConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
